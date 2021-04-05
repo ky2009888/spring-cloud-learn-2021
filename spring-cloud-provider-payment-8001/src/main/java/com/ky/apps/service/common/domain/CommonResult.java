2f8e23c326fef3f8f948778042ec902e4cbad222
ky2009888 <ji_jinliang2012@163.com>
2021-04-05 21:54:37 +0800

    新增支付相关功能

---
+++ spring-cloud-provider-payment-8001/src/main/java/com/ky/apps/service/common/domain/CommonResult.java
@@ -0,0 +1,38 @@
+package com.ky.apps.service.common.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author ky2009666
+ * @Description 通用结果封装处理.
+ * @Date 2021/4/5
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResult<T> {
+    /**
+     * 定义处理结果的状态码.
+     */
+    private Integer code;
+    /**
+     * 定义处理的消息.
+     */
+    private String message;
+    /**
+     * 定义结果数据.
+     */
+    private T data;
+
+    /**
+     * 定义两个参数构造方法.
+     *
+     * @param code    编码.
+     * @param message 消息.
+     */
+    public CommonResult(Integer code, String message) {
+        this(code, message, null);
+    }
+}
