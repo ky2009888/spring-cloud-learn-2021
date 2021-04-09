cdb2f29a9152e3edd9b3b3ff7eef53582df25a99
ky2009888 <ji_jinliang2012@163.com>
2021-04-09 11:31:53 +0800

    代码重构

---
+++ cloud-consumer-order80/src/test/java/com/ky/apps/service/SpringCloudProviderOrder80ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ky.apps.service;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudProviderOrder80ApplicationTests {
+    @Test
+    void testStr(){
+        String name = "name";
+        Assert.assertEquals("name",name);
+    }
+}
