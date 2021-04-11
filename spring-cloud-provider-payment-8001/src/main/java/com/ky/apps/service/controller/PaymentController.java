1d022bbe4931fa5432ab86dfb3bc5674d22304f6
ky2009888 <ji_jinliang2012@163.com>
2021-04-11 17:11:37 +0800

    1、新增EurekaDiscoveryClient 通过它可以打印有哪些服务

--- spring-cloud-provider-payment-8001/src/main/java/com/ky/apps/service/controller/PaymentController.java
+++ spring-cloud-provider-payment-8001/src/main/java/com/ky/apps/service/controller/PaymentController.java
@@ -1,6 +1,9 @@
 package com.ky.apps.service.controller;
 
 import com.ky.apps.service.service.PaymentService;
+import com.netflix.appinfo.InstanceInfo;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
 import lombok.extern.slf4j.Slf4j;
 import om.ky.apps.service.domain.CommonResult;
 import om.ky.apps.service.domain.Payment;
@@ -8,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author ky2009666
@@ -23,6 +27,11 @@ public class PaymentController {
      */
     @Resource
     private PaymentService paymentService;
+    /**
+     * 查看有多少服务.
+     */
+    @Resource
+    private EurekaDiscoveryClient discoveryClient;
     /**
      * 获取服务器的端口号.
      */
@@ -50,5 +59,21 @@ public class PaymentController {
     public CommonResult<Boolean> pay(@RequestBody Payment payment) {
         log.info("支付接口调用的端口号:{}", port);
         return new CommonResult<>(200, "支付成功", paymentService.save(payment));
+    }
+
+    /**
+     * 查看服务相关的信息
+     *
+     * @return
+     */
+    @GetMapping(value = "/discovery")
+    @ResponseBody
+    public Object discovery() {
+        List<ServiceInstance> payment8001 = discoveryClient.getInstances("payment8001");
+        for (ServiceInstance instanceInfo : payment8001
+        ) {
+            log.info("{}", instanceInfo.toString());
+        }
+        return this.discoveryClient;
     }
 }
